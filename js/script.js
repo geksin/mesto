2603125180bf4f2abf497cdcda95275da4068676
Geksin <sxep@yandex.ru>
2020-11-04 17:18:32 +0300

    feat: создал файловую структуру БЭМ

--- js/script.js
+++ js/script.js
@@ -9,9 +9,10 @@ let profileSubtitle = document.querySelector('.popup__profile-subtitile');
 function fillProfileInputs() {
     profileName.value = textTitle.textContent;
     profileSubtitle.value = textSubtitle.textContent;
- //   profileName.innerHTML = '<input id="name" type="text" class="popup__profile-name" value="${textTitle.textContent}">';
- //   profileSubtitle.innerHTML = '<input id="subtitile" type="text" value="${textSubtitle.textContent}" class="popup__profile-subtitile">';
-    console.log(profileName);
+//  хотел понять почему этот код, вместо ка выше не работал, буду рад если подскажите:
+//   profileName.innerHTML = '<input id="name" type="text" class="popup__profile-name" value="${textTitle.textContent}">';
+//   profileSubtitle.innerHTML = '<input id="subtitile" type="text" value="${textSubtitle.textContent}" class="popup__profile-subtitile">';
+//   console.log(profileName);
 }
 
 function showPopup() {
@@ -28,3 +29,15 @@ editButton.addEventListener('click', showPopup);
 closeButton.addEventListener('click', hidePopup);
 
 
+// созданение формы
+let formElement = document.querySelector('.popup__form');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+
+     textTitle.textContent = profileName.value;
+     textSubtitle.textContent = profileSubtitle.value;
+     popup.classList.remove('popup__popup_opened');
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
