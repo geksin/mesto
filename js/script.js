c9778b07e68a5a3bc7d9275a4278af32a3911b42
Geksin <sxep@yandex.ru>
2020-11-08 01:51:51 +0300

    fix:popup,name folder and delete comments

--- js/script.js
+++ js/script.js
@@ -9,19 +9,15 @@ let profileSubtitle = document.getElementById('profile-profession');
 function fillProfileInputs() {
     profileName.value = textTitle.textContent;
     profileSubtitle.value = textSubtitle.textContent;
-//  Спасибо!
-//   profileName.innerHTML = '<input id="name" type="text" class="popup__profile-name" value="${textTitle.textContent}">';
-//   profileSubtitle.innerHTML = '<input id="subtitile" type="text" value="${textSubtitle.textContent}" class="popup__profile-subtitile">';
-//   console.log(profileName);
 }
 
 function showPopup() {
-    popup.classList.add('popup__popup_opened');
+    popup.classList.add('popup_opened');
     fillProfileInputs ();
 }
 
 function hidePopup() {
-    popup.classList.remove('popup__popup_opened');
+    popup.classList.remove('popup_opened');
 }
 
 editButton.addEventListener('click', showPopup);
