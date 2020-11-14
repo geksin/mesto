32f446ffc39cf6f5bc43dbc6a25c91dd36f5ab66
Geksin <sxep@yandex.ru>
2020-11-14 19:44:36 +0300

    add jscode for popup

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 let editButton = document.querySelector('.profile__edit-button');
+let addButton = document.querySelector('.profile__add-button');
 let closeButton = document.querySelector('.popup__button-close');
+let closeButtonAddCard = document.getElementById('popup-close-card');
 let popup = document.querySelector('.popup');
+let popupAdd = document.getElementById('popup-add-card');
 let textTitle = document.querySelector('.profile__name');
 let textSubtitle = document.querySelector('.profile__subtitle');
 let profileName = document.getElementById('profile-name');
@@ -11,6 +14,8 @@ function fillProfileInputs() {
     profileSubtitle.value = textSubtitle.textContent;
 }
 
+
+// Попап редактирования
 function showPopup() {
     popup.classList.add('popup_opened');
     fillProfileInputs ();
@@ -21,8 +26,21 @@ function hidePopup() {
 }
 
 editButton.addEventListener('click', showPopup);
-
 closeButton.addEventListener('click', hidePopup);
+
+// Попап добавления
+
+addButton.addEventListener('click', showPopupAdd);
+closeButtonAddCard.addEventListener('click', hidePopupAdd);
+
+function showPopupAdd() {
+    popupAdd.classList.add('popup_opened');
+    fillProfileInputs ();
+}
+
+function hidePopupAdd() {
+    popupAdd.classList.remove('popup_opened');
+}
 
 
 // созданение формы
@@ -37,3 +55,56 @@ function formSubmitHandler (evt) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+// loading 6 card 
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+
+const container = document.querySelector('.elements');
+
+function createCard(data) {
+    const cardElement = document.querySelector(".card__template").content.cloneNode(true); //создаем элемент из темплейта
+    cardElement.querySelector('.element__name').textContent = data.name; //вставляем данные в карточки беря их из объекта data
+    cardElement.querySelector('.element__photo').src = data.link; //вставляем данные в карточки беря их из объекта data
+    //навешивание обработчиков
+    return cardElement;
+}
+
+function addCardToContainer(data) { //data - это элемент массива
+    const cardElement = createCard(data); 
+    container.append(cardElement);
+}
+
+initialCards.forEach(addCardToContainer);  //функция addCard будет вызвана для каждого элемента массива (forEach так и переводится "для каждого")
+
+
+const button = document.querySelector('.button');
+
+// функция удаления
+
