616a1bf7cccd5f16e1c6ef956d413b7a9a7d5ab8
Geksin <sxep@yandex.ru>
2020-11-16 00:36:11 +0300

    fix:url

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-let editButton = document.querySelector('.profile__edit-button');
-let addButton = document.querySelector('.profile__add-button');
-let closeButton = document.querySelector('.popup__button-close');
-let closeButtonAddCard = document.getElementById('popup-close-card');
-let popup = document.querySelector('.popup');
-let popupAdd = document.getElementById('popup-add-card');
-let textTitle = document.querySelector('.profile__name');
-let textSubtitle = document.querySelector('.profile__subtitle');
-let profileName = document.getElementById('profile-name');
-let profileSubtitle = document.getElementById('profile-profession');
-const addCardName = document.getElementById('card-name');
-const addCardLink = document.getElementById('card-link');
-
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+const closeButton = document.querySelector('.popup__button-close');
+const closeButtonAddCard = document.getElementById('popup-close-card');
+const popup = document.querySelector('.popup');
+const popupAdd = document.getElementById('popup-add-card');
+const textTitle = document.querySelector('.profile__name');
+const textSubtitle = document.querySelector('.profile__subtitle');
+const profileName = document.getElementById('profile-name');
+const profileSubtitle = document.getElementById('profile-profession');
+const inputCardName = document.getElementById('card-name');
+const inputCardLink = document.getElementById('card-link');
+const popupImage = document.querySelector('.popup_image');
 
 
 // Попап редактирования
@@ -33,8 +33,8 @@ closeButtonAddCard.addEventListener('click', hidePopupAdd);
 
 function showPopupAdd() {
     popupAdd.classList.add('popup_opened');
-    addCardName.value = '';
-    addCardLink.value = '';
+    inputCardName.value = '';
+    inputCardLink.value = '';
 }
 
 function hidePopupAdd() {
@@ -42,7 +42,6 @@ function hidePopupAdd() {
 }
 
 // попап картинки 
-const popupImage = document.querySelector('.popup_image');
 
 function showPopupImage() {
     popupImage.classList.add('popup_opened');
@@ -147,8 +146,8 @@ function addCard (evt) {
 
     evt.preventDefault(); 
     const cardElement = document.querySelector('.card__template').content.cloneNode(true); 
-    const cardName = cardElement.querySelector('.element__name').textContent = addCardName.value;
-    const cardLink = cardElement.querySelector('.element__photo').src = addCardLink.value;
+    const cardName = cardElement.querySelector('.element__name').textContent = inputCardName.value;
+    const cardLink = cardElement.querySelector('.element__photo').src = inputCardLink.value;
     // функция лайка
     cardElement.querySelector('.element__button-like').addEventListener('click', function (evt) {
         evt.target.classList.toggle("button-like_yes");
