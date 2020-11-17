e48d5ca4f9a7a3cdd8cfd132aed38d5a350f5472
Geksin <sxep@yandex.ru>
2020-11-17 21:08:21 +0300

    fix:js all comments

--- js/script.js
+++ js/script.js
@@ -12,12 +12,16 @@ const inputCardName = document.getElementById('card-name');
 const inputCardLink = document.getElementById('card-link');
 const popupImage = document.querySelector('.popup_image');
 
-
+//Спасибо за правильный нейминг, где-то можно прочитать рекомендации? 
 
 function showPopup(popup) {
     popup.classList.add('popup_opened');
 }
 
+function fillProfileInputs() {
+    profileNameInput.value = userName.textContent;
+    profileDescriptionInput.value = userDescription.textContent;
+}
 
 function showEditUserProfilePopup(popup) {
     showPopup(popup);
@@ -33,7 +37,6 @@ function hidePopup(popup) {
 openEditProfilePopupButton.addEventListener('click', () => showEditUserProfilePopup(popupEditUserProfile));
 closeEditProfilePopupButton.addEventListener('click', () => hidePopup(popupEditUserProfile));
 
-// Попап добавления
 
 openAddCardPopupButton.addEventListener('click', () => showAddCardPopup(popupAddCard));
 closeAddCardPopupButton.addEventListener('click', () => hidePopup(popupAddCard));
@@ -44,24 +47,9 @@ function showAddCardPopup(popup) {
     inputCardLink.value = '';
 }
 
-// попап картинки 
 
-function showPopupImage() {
-    popupImage.classList.add('popup_opened');
-}
+popupImage.querySelector('.popup__button-close').addEventListener('click',() => hidePopup(popupImage));
 
-function hidePopupImage() {
-    popupImage.classList.remove('popup_opened');
-}
-popupImage.querySelector('.popup__button-close').addEventListener('click',hidePopupImage);
-
-
-// созданение формы
-
-function fillProfileInputs() {
-    profileNameInput.value = userName.textContent;
-    profileDescriptionInput.value = userDescription.textContent;
-}
 
 const formElement = document.querySelector('.popup__form');
 
@@ -70,39 +58,10 @@ function formSubmitHandler (evt) {
 
      userName.textContent = profileNameInput.value;
      userDescription.textContent = profileDescriptionInput.value;
-     hidePopup ();
+     hidePopup (popupEditUserProfile);
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
-
-
-// loading 6 card 
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
 
 // функция добавления 6 карточек
 const container = document.querySelector('.elements');
@@ -123,17 +82,12 @@ function createCard(data) {
 
 // Открытие попапа с картинкой
 function openImagePopup (link,text) {
-    const popupImage = document.querySelector('.popup_image');
-    popupImage.querySelector('.popup__img').src = link;
-    popupImage.querySelector('.popup__img').title = text;
+    const imgpopupImage = popupImage.querySelector('.popup__img');
+    imgpopupImage.src = link;
+    imgpopupImage.title = text;
+    imgpopupImage.alt = "Фотография: " + text;
     popupImage.querySelector('.popup__signature').textContent = text;
-    popupImage.classList.add('popup_opened');
-}
-
-// закрытие попапа с картинкой
-
-function closeImagePopup () {
-    popupImage.classList.remote('popup_opened');
+    showPopup(popupImage);
 }
 
 function addCardToContainer(data) {
@@ -143,30 +97,20 @@ function addCardToContainer(data) {
 
 initialCards.forEach(addCardToContainer); 
 
-// функция добавления карточки
+// Привет! Спасибо за замечания, переделал функцию, убрал дубляж кода, но как полностью удалить не сообразил. Там есть разница в том, нужно карточку в начало поместить. 
 
-function addCard (evt) {
-
-    evt.preventDefault(); 
-    const cardElement = document.querySelector('.card__template').content.cloneNode(true); 
-    const cardName = cardElement.querySelector('.element__name').textContent = inputCardName.value;
-    const cardLink = cardElement.querySelector('.element__photo').src = inputCardLink.value;
-    // функция лайка
-    cardElement.querySelector('.element__button-like').addEventListener('click', function (evt) {
-        evt.target.classList.toggle("button-like_yes");
-    });
-    // функция удаления
-    cardElement.querySelector('.element__button-delete').addEventListener('click', function (evt) {
-        evt.target.parentElement.remove();
-    });
-    // попап при нажатии 
-    cardElement.querySelector('.element__photo').addEventListener('click', (evt) => openImagePopup (cardLink, cardName));
-
+function addCardToContainerStart (evt) {
+    evt.preventDefault();
+    const cardData = {
+        name: inputCardName.value,
+        link: inputCardLink.value
+     }
+    const cardElement = createCard(cardData);
     container.prepend(cardElement);
-    hidePopupAdd ();
+    hidePopup (popupAddCard);
 }
 
-popupAddCard.addEventListener('submit', addCard);
+popupAddCard.addEventListener('submit', addCardToContainerStart);
 
 
 
